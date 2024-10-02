ec7a7cec73138e0721cff3e1ef9d6f52b6eab31f
mushymato <wildshinobu@gmail.com>
2024-10-02 10:13:47 -0400

    Sometimes output and trigger id can be null, unsure what mods do this

--- MachineControlPanel/Framework/Quirks.cs
+++ MachineControlPanel/Framework/Quirks.cs
@@ -13,29 +13,44 @@ namespace MachineControlPanel.Framework
         /// <param name="getId"></param>
         /// <param name="setIdSeq"></param>
         /// <param name="process"></param>
-        private static void EnsureUniqueId<T>(List<T>? modelList, Func<T, string> getId, Action<T, int> setIdSeq, Action<T>? process = null)
+        private static void EnsureUniqueId<T>(string debugText, List<T>? modelList, Func<T, string> getId, Action<T, int> setIdSeq, Action<T>? process = null)
         {
             if (modelList == null)
                 return;
+            List<T> nullIdModels = [];
             Dictionary<string, List<T>> modelById = [];
             foreach (T model in modelList)
             {
                 string id = getId(model);
-                if (!modelById.ContainsKey(id))
-                    modelById[id] = [];
-                modelById[id].Add(model);
-                process?.Invoke(model);
+                if (id == null)
+                    nullIdModels.Add(model);
+                else
+                {
+                    if (!modelById.ContainsKey(id))
+                        modelById[id] = [];
+                    modelById[id].Add(model);
+                    process?.Invoke(model);
+                }
             }
 
+            int i;
             foreach ((string key, List<T> models) in modelById)
             {
                 if (models.Count > 1)
                 {
                     // edit second duplicate and on
-                    int i = 1;
+                    i = 1;
                     foreach (T model in models.Skip(1))
                         setIdSeq(model, i++);
                 }
+            }
+            // unsure when model could have null id, log for future detection
+            if (nullIdModels.Any())
+                ModEntry.LogOnce($"{debugText} has null Ids");
+            i = 0;
+            foreach (T model in nullIdModels)
+            {
+                setIdSeq(model, i++);
             }
         }
 
@@ -50,15 +65,17 @@ namespace MachineControlPanel.Framework
             foreach ((string qItemId, MachineData machine) in data)
             {
                 EnsureUniqueId(
+                    qItemId,
                     machine.OutputRules,
                     (rule) => rule.Id,
-                    (rule, seq) => rule.Id = $"{rule.Id}-rule-{seq}",
-                    static (rule) =>
+                    (rule, seq) => rule.Id = rule.Id == null ? $"null-rule-{seq}" : $"{rule.Id}-rule-{seq}",
+                    (rule) =>
                     {
                         EnsureUniqueId(
+                            $"{qItemId}-{rule.Id}",
                             rule.Triggers,
                             (trigger) => trigger.Id,
-                            (trigger, seq) => trigger.Id = $"{trigger.Id}-trigger-{seq}"
+                            (trigger, seq) => trigger.Id = trigger.Id == null ? $"null-trigger-{seq}" : $"{trigger.Id}-trigger-{seq}"
                         );
                     }
                 );
